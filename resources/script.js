e196e4f79989dd3e3e82a5030bd7960105d76e97
StianKorsmo <s354560@oslomet.no>
2020-11-24 12:27:27 +0100

    :)

--- resources/script.js
+++ resources/script.js
@@ -7,7 +7,7 @@ window.onload = function() {oppstart()};
 window.onscroll = function() {topButton()};
 
     function topButton(){
-        if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+        if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
             document.getElementById("btnTop").style.display = "block"; 
         }else{
             document.getElementById("btnTop").style.display = "none";
